3a1f63e1161381b7eec30ee1b2c41b7d821697ba
Fenna <fennnakadir@outlook.com>
2023-05-01 16:44:20 +0100

    version payload

--- bitcoinexplorer.py
+++ bitcoinexplorer.py
@@ -1,18 +1,34 @@
 import socket
+import time
+import struct 
+import hashlib
 
-# Establish connection
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 8333))
+# # Establish connection
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.connect(('127.0.0.1', 8333))
 
 # Send version message
-version_payload = b"\xf9\xbe\xb4\xd9" + \
-                  b"version\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x7f\x00\x00\x01\x20\x8d" + \
-                  b"\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + \
-                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+version = 70015  # Replace with your own version number
+services = 0  # Replace with the services you want to offer
+timestamp = int(time.time())  # Replace with the current Unix timestamp
+addr_recv = ("0.0.0.0", 8333, 0)  # Replace with the address of the receiving node
+addr_from = ("0.0.0.0", 8333, 0)  # Replace with your own address
+nonce = 1234567890  # Replace with a random nonce value
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"  # Replace with your own user agent string
+user_agent = user_agent.encode()
+start_height = 0  # Replace with the block height of the latest block you have
+
+# Create the payload message
+payload = struct.pack("<iQq26s26sQsQ", version, services, timestamp, addr_recv, addr_from, nonce, user_agent, start_height)
+
+# Define the magic value and command name
+magic = b"\xf9\xbe\xb4\xd9"
+command = b"version\x00\x00\x00\x00\x00\x00\x00\x00"
+
+# Calculate the payload checksum
+payload_checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
+
+# Create the complete version message
+message = magic + command + struct.pack("<I", len(payload)) + payload + payload_checksum
+
+print(message)
