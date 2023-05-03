092c245966bb169099a29f5353093b58e81b51c3
Fenna <fennnakadir@outlook.com>
2023-05-03 16:59:13 +0100

    unpack header, message verification

--- bitcoinexplorer.py
+++ bitcoinexplorer.py
@@ -1,9 +1,9 @@
 # Import dependencies
 import socket
 import time
-import struct 
-import hashlib
 import random
+import struct
+import hashlib
 import binascii
 
 # Binary encode the sub-version
@@ -56,8 +56,8 @@ def print_response(command, request_data, response_data):
     print(binascii.hexlify(response_data))
 
 def parse_inv_payload(payload):
-    
     num_items = struct.unpack("<I", payload[:4])[0]
+    print(num_items)
 
     items = []
     for i in range(num_items):
@@ -70,6 +70,24 @@ def parse_inv_payload(payload):
     for item in items:
         print(f"Item type: {item[0]}, Item hash: {item[1].hex()}")
 
+def unpack_header(response):
+    # Define the header format
+    header_fmt = "<4s12sI4s"
+
+    # Unpack the header fields from the message
+    magic, com, payload_size, checksum = struct.unpack(header_fmt, response[:24])
+
+    # Format command 
+    command = com.decode('ascii').strip('\0')
+
+    # Print the header fields
+    print(f"Magic: {magic.hex()}")
+    print(f"Command: {command}")
+    print(f"Payload size: {payload_size}")
+    print(f"Checksum: {checksum.hex()}")
+    print("\n")
+    return command  
+
 def main():
     # Constants
     magic_value = 0xd9b4bef9
@@ -81,38 +99,38 @@ def main():
     # Create Request Objects
     version_payload = create_payload_version(node_ip_address)
     version_message = create_message(magic_value, 'version', version_payload)
-    verack_message = create_message_verack()
-    getdata_payload = create_payload_getdata(tx_id)
-    getdata_message = create_message(magic_value, 'getdata', getdata_payload)
 
-        # Establish TCP Connection
+    verack_message = create_message_verack()
+
+    # Establish TCP Connection
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((node_ip_address, port))
 
     # Send message "version"
     sock.send(version_message)
     response_data = sock.recv(buffer_size)
-    print_response("version", version_message, response_data)
+    # print_response("version", version_message, response_data)
 
     # Send message "verack"
     sock.send(verack_message)
     response_data = sock.recv(buffer_size)
-    print_response("verack", verack_message, response_data)
+    # print_response("verack", verack_message, response_data)
 
-    # while True:
-    #     data = sock.recv(1024)
-    #     payload = binascii.hexlify(data)
-    #     # Make sure its a valid payload message
-    #     if payload[:8] == b'f9beb4d9':
-    #         parse_inv_payload(binascii.hexlify(data))
+    while True: 
+        data = sock.recv(65536)
+        if len(data) > 24:
+            command = unpack_header(data)
+            if (command=='inv'):
+                print(data)
+                parse_inv_payload(data[24:]) 
+    
+    # # Send message "getdata"
+    # sock.send(getdata_message)
+    # response_data = sock.recv(buffer_size)
+    # print_response("getdata", getdata_message, response_data)
 
-    # Send message "getdata"
-    sock.send(getdata_message)
-    response_data = sock.recv(buffer_size)
-    print_response("getdata", getdata_message, response_data)
-
-    # Close the TCP connection
-    sock.close()
+    # # Close the TCP connection
+    # sock.close()
 
 
 main()
