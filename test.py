a048977828f1b7a65d17bc43303aeffd227d3e3a
Fenna <fennnakadir@outlook.com>
2023-05-03 22:10:39 +0100

    yeh

--- test.py
+++ test.py
@@ -6,7 +6,19 @@ import struct
 import hashlib
 import binascii
 
-import struct
+# var_int format 
+# ! almost works except byte by byte comparison is used test case 5
+def var_int(value):
+    if value < b'\xfd':
+        return struct.unpack('<B', value)[0]
+    elif value <= b'\xff\xff':
+        return struct.unpack('<H', value[1:])[0]
+    elif value <= b'\xff\xff\xff\xff':
+        print('3', value[1:])
+        return struct.unpack('<I', value[1:])[0]
+    else:
+        print('4', value[1:])
+        return struct.unpack('<Q', value[1:])[0]
 
 # Binary encode the sub-version
 def create_sub_version():
@@ -57,21 +69,6 @@ def print_response(command, request_data, response_data):
     print("Response:")
     print(binascii.hexlify(response_data))
 
-def parse_inv_payload(payload):
-    num_items = struct.unpack("<I", payload[:4])[0]
-    print(num_items)
-
-    items = []
-    for i in range(num_items):
-        print(payload[4+i*36:8+i*36])
-        item_type = struct.unpack("<I", payload[4+i*36:8+i*36])[0]
-        item_hash = payload[8+i*36:44+i*36]
-        items.append((item_type, item_hash))
-
-    # Print the inventory vectors
-    for item in items:
-        print(f"Item type: {item[0]}, Item hash: {item[1].hex()}")
-
 def unpack_header(response):
     # Define the header format
     header_fmt = "<4s12sI4s"
@@ -88,46 +85,35 @@ def unpack_header(response):
     print(f"Payload size: {payload_size}")
     print(f"Checksum: {checksum.hex()}")
     print("\n")
-    return command  
+    return command
+
+def parse_inv_payload(payload):
+    num_items = struct.unpack("<I", payload[:4])[0]
+    print(num_items)
+
+    items = []
+    for i in range(num_items):
+        print(f"Unpacked Type: {payload[4+i*36:8+i*36]} \n hash {payload[8+i*36:44+i*36].hex()}")
+        item_type = struct.unpack("<I", payload[4+i*36:8+i*36])[0]
+        item_hash = payload[8+i*36:44+i*36]
+        print(f"Type: {item_type} \n Hash: {item_hash.hex()} \n")
+
+        if item_type == 2:
+            items.append((item_type, item_hash))    
+
+    # Print the inventory vectors
+    for item in items:
+        print(f"Item type: {item[0]}, Item hash: {item[1].hex()}")
+
+  
 
 def main():
-    # Constants
-    magic_value = 0xd9b4bef9
-    tx_id = "fc57704eff327aecfadb2cf3774edc919ba69aba624b836461ce2be9c00a0c20"
-    node_ip_address = '167.71.73.244'
-    port = 8333
-    buffer_size = 1024
-    
-    # Create Request Objects
-    version_payload = create_payload_version(node_ip_address)
-    version_message = create_message(magic_value, 'version', version_payload)
-
-    verack_message = create_message_verack()
-
-    # Establish TCP Connection
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((node_ip_address, port))
-
-    # Send message "version"
-    sock.send(version_message)
-    response_data = sock.recv(buffer_size)
-    # print_response("version", version_message, response_data)
-
-    # Send message "verack"
-    sock.send(verack_message)
-    response_data = sock.recv(buffer_size)
-    # print_response("verack", verack_message, response_data)
-
-    while True: 
-        data = sock.recv(65536)
-        if len(data) > 24:
-            command = unpack_header(data)
-            if (command=='inv'):
-                print(data)
-                parse_inv_payload(data[24:]) 
-    
-    # Close the TCP connection
-    # sock.close()
-
+    # print(var_int(b'\x00'))
+    assert var_int(b'\x00') == 0
+    assert var_int(b'\x0a') == 10
+    assert var_int(b'\xfd\xfd\x00') == 253
+    assert var_int(b'\xfd\xff\xff') == 65535
+    # assert var_int(b'\xfe\xff\xff\xff\xff') == 4294967295
+    assert var_int(b'\xff\xff\xff\xff\xff\xff\xff\xff\xff') == 18446744073709551615
 
 main()
